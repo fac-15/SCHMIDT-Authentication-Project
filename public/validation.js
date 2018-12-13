f0ac838e53c84eacdf4dc38dfc6122380420a729
mike <boimik87@gmail.com>
2018-12-13 20:25:29 +0000

    home button in login page now redirects HOME

---
+++ public/validation.js
@@ -0,0 +1,7 @@
+// const validate = (username, password) => {
+//   let username = document.getElementById("login_username").value;
+//   let password = document.getElementById("login_password").value;
+
+//  if ()
+
+// };
